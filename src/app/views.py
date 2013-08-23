1f07c26107257c775f64f80441230f80b4419b9c
Robin Gottfried <czervenka@gmail.com>
2013-08-23 13:37:23 +0200

    added jinja2 templates and default static directory

--- src/app/views.py
+++ src/app/views.py
@@ -1,9 +1,12 @@
 from utils.decorators import as_view
+from app.template import get_template
 
 
 @as_view
 def welcome_screen(request, response):
-    return "It works!"
+    return get_template("homepage.html").render({
+        'project_name': 'Example project',
+    })
 
 @as_view
 def not_found(request, response, *args, **kwargs):
--- src/config.py
+++ src/config.py
@@ -1,1 +1,11 @@
+import os
+
+
+ROOT_PATH = os.path.dirname(__file__)
+
+
 DEBUG = True
+
+TEMPLATES_PATH = os.path.join(ROOT_PATH, 'templates')
+STATIC_PATH = os.path.join(ROOT_PATH, 'static')  # needs to point to the same path as in app.yaml
+STATIC_URL = '/static'  # needs to point to the same url as in app.yaml
