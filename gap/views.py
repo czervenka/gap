dd17efa0a5c97c1705fd46791ae4239ee484f07e
Robin Gottfried <czervenka@gmail.com>
2013-09-22 10:46:29 +0200

    not found page now shows list of routes if settings['debug'] is set

--- gap/views.py
+++ gap/views.py
@@ -1,5 +1,6 @@
 from utils.decorators import as_view
 from gap.template import get_template
+from StringIO import StringIO
 
 
 @as_view
@@ -8,7 +9,34 @@ def welcome_screen(request, response):
         'project_name': 'Example project',
     })
 
+
 @as_view
 def not_found(request, response, *args, **kwargs):
     response.set_status(404)
-    return 'The page you are requesting was not found on this server.'
+    text = 'The page you are requesting was not found on this server.'
+
+    from gap.conf import settings
+    if settings['DEBUG']:
+        buffer = StringIO()
+        dump_routes(buffer)
+        buffer.seek(0)
+        return text + '<br>The known rotes are...<br><pre>' + buffer.read() + '</pre>'
+    else:
+        return text
+
+def dump_routes(response, routes=None, indent=''):
+    if routes is None:
+        from routes import routes
+    for route in routes:
+        dump_route(response, route, indent)
+
+def dump_route(response, route, indent=''):
+    if isinstance(route, (tuple, list)):
+        response.write(route[0] + ' -> ' + route[1] + '\n')
+    elif hasattr(route, 'routes'):
+        response.write(indent + route.prefix + ':' + '\n')
+        dump_routes(response, route.routes, indent+'  ')
+    elif hasattr(route, 'regex'):
+        response.write(indent + route.regex.pattern + ' -> ' + repr(route.handler) + '\n')
+    else:
+        raise TypeError('Unknown route class %s' % route.__class__.__name__)
