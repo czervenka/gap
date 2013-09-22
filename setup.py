d61e1d0cf3550f7b953ec44b41660fa8500ae7ac
Robin Gottfried <czervenka@gmail.com>
2013-09-22 11:22:37 +0200

    version in setup raised

--- setup.py
+++ setup.py
@@ -15,11 +15,12 @@
 #
 # @author Robin Gottfried <google@kebet.cz>
 # part of gap project (https://github.com/czervenka/gap)
+__version__ = '0.4'
+
 import os
 from setuptools import setup, findall
 from collections import defaultdict
 
-__version__ = '0.3'
 
 try:
     import multiprocessing
