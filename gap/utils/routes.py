d074340fa94901f0025c4ea0e5334fd571c0e515
Robin Gottfried <czervenka@gmail.com>
2013-09-29 11:09:52 +0200

    new RouteEx with changes I like / need in my project

--- gap/utils/routes.py
+++ gap/utils/routes.py
@@ -18,6 +18,7 @@
 
 from types import StringTypes
 
+import webapp2
 from webapp2_extras.routes import PathPrefixRoute
 from webapp2 import Router
 from gap.utils.imports import import_class
@@ -32,3 +33,26 @@ def include(prefix, routes):
     routes = [ Router.route_class(*route) if isinstance(route, tuple) else route for route in routes ]
 
     return PathPrefixRoute(prefix, routes)
+
+
+class RouteEx(webapp2.Route):
+    '''
+    Adds strict_slash parameter which allows to control wheter urls with and
+    without slash will lead to the same handler.
+    '''
+
+    def __init__(self, *args, **kwargs):
+        self._strict_slash = kwargs.pop('strict_slash', False)
+        super(RouteEx, self).__init__(*args, **kwargs)
+        if not self._strict_slash:
+            self.template += '<__slash:/?>'
+
+    def match(self, request):
+        res = super(RouteEx, self).match(request)
+        if res and not self._strict_slash:
+            self, args, kwargs = res
+            kwargs.pop('__slash')
+            res = (self, args, kwargs)
+        return res
+
+
